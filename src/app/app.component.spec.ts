380ea01392cebdd5baef78007840831ec47c7cfd
Rohan Deshwal <rohandeshwal19@gmail.com>
2025-05-12 12:25:01 +0530

    version changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,16 +19,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'energyMinds' title`, () => {
+  it(`should have the 'energyMind' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('energyMinds');
+    expect(app.title).toEqual('energyMind');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, energyMinds');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, energyMind');
   });
 });
